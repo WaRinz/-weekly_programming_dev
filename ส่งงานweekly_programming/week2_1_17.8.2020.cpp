2daecef1c96c8a4d00401d54750aeb9b23074787
WaRinz <seleniumdichloride@gmail.com>
2020-08-21 12:23:31 +0700

    week_2work_1

--- ส่งงานweekly_programming/week2_1_17.8.2020.cpp
+++ ส่งงานweekly_programming/week2_1_17.8.2020.cpp
@@ -1,3 +1,4 @@
+/*63010849 WORAWICH TANGPOJTHAWEEPORN SEC119*/
 #include<stdio.h>
 /*AREA OF CIRCLE*/
 double circle(double r)
@@ -31,7 +32,7 @@ int main()
 	printf("1-> CIRCLE \n2-> RECTANGLE \n3-> TRIANGLE \n\npress the number then ENTER ---> ");
 	scanf_s("%d", &num);
 /*IF INPUT NUMBER IS 1,2,3 CAN GO TO SOLUTION*/
-if (num == 1 || 2 || 3) 
+	if (num == 1 || 2 || 3) 
 	{
 /*PRESS 1 -> GO TO CIRCLE*/
 		if (num == 1)
@@ -39,7 +40,15 @@ if (num == 1 || 2 || 3)
 			printf("-- CIRCLE --\n");
 			printf("ENTER RADIUS : ");
 			scanf_s("%lf", &r);
-			printf("AREA OF CIRCLE IS : %lf\n\n", circle(r));
+	/*Area cannot less than zero*/
+			if (r < 0)
+			{
+				printf("!! Area cannot less than zero !!\n\n");
+			}
+			if (r > 0)
+			{
+				printf("AREA OF CIRCLE IS : %lf\n\n", circle(r));
+			}
 			printf("--- END ---\n\n");
 
 		}
@@ -51,7 +60,15 @@ if (num == 1 || 2 || 3)
 			scanf_s("%lf", &w);
 			printf("ENTER HEIGHT : ");
 			scanf_s("%lf", &h);
-			printf("AREA OF RECTANGLE IS : %lf\n\n", rectangle(w, h));
+	/*Area cannot less than zero*/
+			if (w * h < 0)
+			{
+				printf("!! Area cannot less than zero !!\n\n");
+			}
+			if (w * h > 0) 
+			{
+				printf("AREA OF RECTANGLE IS : %lf\n\n", rectangle(w, h));
+			}
 			printf("--- END ---\n\n");
 		}
 /*PRESS 3 -> GO TO TRIANGLE*/
@@ -62,13 +79,20 @@ if (num == 1 || 2 || 3)
 			scanf_s("%lf", &w);
 			printf("ENTER HEIGHT : ");
 			scanf_s("%lf", &h);
-			printf("AREA OF RECTANGLE IS : %lf\n\n", triangle(w, h));
+	/*Area cannot less than zero*/
+			if (w * h < 0)
+			{
+				printf("!! Area cannot less than zero !!\n\n");
+			}
+			if (w * h > 0) {
+				printf("AREA OF TRIANGLE IS : %lf\n\n", triangle(w, h));
+			}
 			printf("--- END ---\n\n");
 		}
 /*IF INPUT NUMBER IS NOT 1,2,3 --> ERROR CAN'T PROCESS*/
 		else
 		{
-			printf("\n\n** ERROR! TYPE 1,2,3 ONLY!!! **\n\n");
+			printf("\n\n** ERROR! SELECT MENU 1,2,3 ONLY!!! **\n\n");
 		}
 	}
 /*END LINE*/
