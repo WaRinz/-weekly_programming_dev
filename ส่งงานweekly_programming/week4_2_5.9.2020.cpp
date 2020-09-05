dd094d0271f552af6902ebd3f1173a1e3e03a737
WaRinz <69125950+WaRinz@users.noreply.github.com>
2020-09-05 15:36:08 +0700

    Add files via upload

---
+++ ส่งงานweekly_programming/week4_2_5.9.2020.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+void menu();
+void contact();
+void menu()
+{
+	printf("- Cha nom yen		35.- \n");
+	printf("- Green Tea 		40.- \n");
+	printf("- Mocca			40.- \n");
+	printf("- Latte			40.- \n");
+	printf("- Strawberry Cake	50.- \n");
+	printf("- Cheese Cake		60.- \n");
+	printf("- Hot Chocolate		50.- \n");
+	printf("- Srawberry Milk	40.- \n");
+	printf("- Honey Pancake		85.- \n\n");
+
+}
+void contact() 
+{
+	printf("555/555 Moon road 20100 \n@loonatheword Tel : 084-5965875\n\n");
+}
+int main()
+{
+	int num;
+	printf("== HAPPY DRINK ONLINE CAFE ==\n\n");
+	printf("Select Menu : \n");
+	printf("1-> Menu \n2.Contact  \n\n==>");
+	scanf_s("%d", &num);
+	if (num == 1 || 2)
+	{
+		/*go to menu*/
+		if (num == 1)
+		{
+			menu();
+		}
+		/*go to contact*/
+		else if (num == 2)
+		{
+			contact();
+		}
+		else /*type else*/
+		{
+			printf("Please Enter 1,2 ONLY!!\n\n");
+		}
+	}
+
+	printf("THANK YOU!\n");
+	/*end line*/
+	printf("==================================");
+	return 0;
+}
