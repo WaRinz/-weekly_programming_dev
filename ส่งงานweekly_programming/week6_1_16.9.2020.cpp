810b679fa2afeeb711aeca31bb87fdfe682c9e82
WaRinz <69125950+WaRinz@users.noreply.github.com>
2020-09-16 11:44:00 +0700

    Update week6_1_16.9.2020.cpp

--- ส่งงานweekly_programming/week6_1_16.9.2020.cpp
+++ ส่งงานweekly_programming/week6_1_16.9.2020.cpp
@@ -8,30 +8,31 @@ int age()
 	/* address */
 	p = &a;
 	scanf_s("%d", &a);
-	if ((*p >= 1))
+	if (*p)
 	{
-		if (a <= 11)
+
+		if (a <= 11 && a > 0)
 		{
 			printf("%d -> You are Kid", *p);
 		}
-		else if (a <= 20)
+		else if (a <= 20 && a > 0)
 		{
 			printf("%d -> You are Teen", *p);
 		}
-		else if (a <= 60)
+		else if (a <= 60 && a > 0)
 		{
 			printf("%d -> You are Adult", *p);
 		}
-		else if (a <= 130)
+		else if (a <= 130 && a > 0)
 		{
 			printf("%d -> You are Old", *p);
 		}
 		else
 		{
-			printf("!!CAUTIONS!!\n\nAge must start at 1 and can not be characters!\n(normal people can't live more than 130 years!)");
+			printf("!!CAUTIONS!!\n\nAge must start at 0 and can not be characters!\n(normal people can't live more than 130 years!)");
 		}
 	}
-	
+
 	return *p;
 }
 int main()
