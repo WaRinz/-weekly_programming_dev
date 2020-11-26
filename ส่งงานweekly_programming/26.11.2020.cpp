65b21f1cdcba7eb105df54d3a97287f00ff85da6
WaRinz <69125950+WaRinz@users.noreply.github.com>
2020-11-26 22:05:46 +0700

    Add files via upload

---
+++ ส่งงานweekly_programming/26.11.2020.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+// function
+void alphabet();
+
+void alphabet()
+{
+    // array
+    char alph[27];
+    int x;
+
+    // pointer
+    char* ptr;
+    ptr = alph;
+    printf("----------------------------------------\n");
+
+
+
+    for (x = 0; x < 26; x++)
+    {
+        *ptr = x + 'A';
+        ptr++;
+    }
+    ptr = alph;
+
+    printf(" The Alphabets are : \n");
+    for (x = 0; x < 26; x++)
+    {
+        printf(" %c ", *ptr);
+        ptr++;
+    }
+    printf("\n\n");
+}
+int main()
+{
+    int choice;
+
+    // Input & Output
+    printf("\n\n Pointer : Print all the alphabets:\n");
+    printf("----------------------------------------\n");
+    printf(" Print or Close program ?");
+    printf("\n Print = 1 / Close = 2 ( NO SPACEBAR )");
+    printf("\n ---> ");
+    scanf_s("%d", &choice);
+
+    // Expression " or "  " If " Condition
+    if (choice == 1 || choice == 2)
+    {
+        // if in if
+        if (choice == 1)
+        {
+            alphabet();
+        }
+        else if (choice == 2)
+        {
+            printf(" CLOSE PROGRAM");
+        }
+    }
+
+    else
+    {
+        printf(" Please Try Again ---> Type 1 or 2 only");
+    }
+
+    printf("\n\n THANK YOU!!");
+    printf("\n\n========================================\n");
+    return(0);
+}
